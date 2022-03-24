79bc4f9e4c76e231ff3e5be851c7d53bd8dbd15f
DekuDeshayan <buraimoRalph129@gmail.com>
2022-03-24 14:52:12 +0200

    comentario runner teste

--- src/main/java/com/sha/springbootdeviceseller/SpringBootDeviceSellerApplication.java
+++ src/main/java/com/sha/springbootdeviceseller/SpringBootDeviceSellerApplication.java
@@ -9,5 +9,7 @@ public class SpringBootDeviceSellerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDeviceSellerApplication.class, args);
 	}
+	
+	//runner
 
 }
